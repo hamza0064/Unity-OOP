ee714ed385332d56138aec28556efd89bb64df2f
Hamza <hamza.mumtaz5@gmail.com>
2025-06-02 15:41:02 +0500

    Playermovement

--- oop-Learning/Assets/KitchenGame/Script/inputManager.cs
+++ oop-Learning/Assets/KitchenGame/Script/inputManager.cs
@@ -1,19 +1,23 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
-public class inputManager : MonoBehaviour
+
+namespace kitchen_Project
 {
-
-    private Input_Manage playerinputactions;
-    private void Awake()
+    public class inputManager : MonoBehaviour
     {
-        playerinputactions = new Input_Manage();
-        playerinputactions.Player.Enable();
-    }
 
-    public Vector2 GetMovementVectorNormalize()
-    {
-        Vector2 inputVector = playerinputactions.Player.Move.ReadValue<Vector2>();
-        inputVector = inputVector.normalized;
-        return inputVector;
+        private Input_Manage playerinputactions;
+        private void Awake()
+        {
+            playerinputactions = new Input_Manage();
+            playerinputactions.Player.Enable();
+        }
+
+        public Vector2 GetMovementVectorNormalize()
+        {
+            Vector2 inputVector = playerinputactions.Player.Move.ReadValue<Vector2>();
+            inputVector = inputVector.normalized;
+            return inputVector;
+        }
     }
 }
--- oop-Learning/Assets/KitchenGame/Script/player.cs
+++ oop-Learning/Assets/KitchenGame/Script/player.cs
@@ -12,7 +12,49 @@ namespace kitchen_Project
             Vector2 inputVector = inputmanager.GetMovementVectorNormalize();
 
             Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
-            transform.position += moveDir * movespeed * Time.deltaTime;
+
+            float moveDistance = movespeed * Time.deltaTime;
+            float playerRadius = 0.7f;
+            float playerHeight = 2f;
+            bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDir , moveDistance);
+
+            // // Draw capsule direction as a line (just for visualization)
+            // Color rayColor = canMove ? Color.green : Color.red;
+            // Vector3 rayStart = transform.position + Vector3.up * (playerHeight / 2);
+            // Vector3 rayEnd = rayStart + moveDir.normalized * moveDistance;
+
+            // // Scene view: Draw the full movement direction
+            // Debug.DrawLine(rayStart, rayEnd, rayColor, 0.3f);
+
+            // Debug.Log(canMove);
+            if (!canMove)
+            {
+                Vector3 moveDirX = new Vector3(moveDir.x, 0, 0).normalized;
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
+
+                if (canMove)
+                {
+                    moveDir = moveDirX;
+                }
+                else
+                {
+                    Vector3 moveDirZ = new Vector3(0, 0, moveDir.z).normalized;
+                    canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
+                    if (canMove)
+                    {
+                        moveDir = moveDirZ;
+                    }
+                    else
+                    {
+                        
+                    }
+                }
+            }
+
+            if (canMove)
+            {
+                transform.position += moveDir * movespeed * Time.deltaTime;
+            }
 
             float rotSpeed = 10f;
             transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotSpeed);
