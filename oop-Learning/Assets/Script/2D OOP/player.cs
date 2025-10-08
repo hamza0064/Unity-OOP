08c3f2f954e154566b294f4fa9aad57cd7e172fe
Hamza <hamza.mumtaz5@gmail.com>
2025-10-09 02:24:25 +0500

    Run + Idle Animation
    add new model and apply basic animation on it also control animation from code

--- oop-Learning/Assets/Script/2D OOP/player.cs
+++ oop-Learning/Assets/Script/2D OOP/player.cs
@@ -4,11 +4,12 @@ using UnityEngine.TextCore.Text;
 
 public class player : character_Class
 {
+    public Animator anim;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -27,6 +28,21 @@ public class player : character_Class
 
         float rotSpeed = 10f;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotSpeed);
+        float currentSpeed = moveDir.magnitude;
+        if (anim != null)
+        {
+            // “Speed” float parameter in Animator (0 = idle, 1 = running)
+            
+            anim.SetFloat("Run", currentSpeed);
+        }
+
+        foreach (Transform child in transform)
+        {
+            if (currentSpeed <= 0.01f)
+                child.localRotation = Quaternion.Euler(0f, 45f, 0f);   // Idle
+            else
+                child.localRotation = Quaternion.Euler(0f, 20f, 0f);   // Run
+        }
     }
 
 }
